010384312f9a7e31cd577b5777c5fa1cc40da293
Nikhil Ambure <nikhilambure25@gmail.com>
2024-10-17 20:18:48 +0530

    Update settings.py

--- Todo_List/settings.py
+++ Todo_List/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uj#*=6cu@on+uyv0^33l$n&4*dx-e9r@)cw^v4&o*l2zuar8+&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False    
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
